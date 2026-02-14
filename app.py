9234971dce0b3a357530baa2eaaee72f4edd66d4
kamalclassmumbai <130304049+kamalclassmumbai@users.noreply.github.com>
2026-02-14 13:02:48 +0530

    fix gunicorn

--- app.py
+++ app.py
@@ -18,10 +18,10 @@ def home():
 		return render_template("home.html")
 
 
-port = int(os.environ.get("PORT", 5000))
+#port = int(os.environ.get("PORT", 5000))
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=port)
+#if __name__ == "__main__":
+#    app.run(host="0.0.0.0", port=port)
 
 
 
